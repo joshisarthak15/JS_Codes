6c9792228da717911bfc4f2484151b27eeee3ea5
joshisarthak15 <113760347+joshisarthak15@users.noreply.github.com>
2024-08-22 12:30:26 +0000

    container config done

---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+let accountPass = "1234567"
+var accountName = "Sarthak Joshi"
+const accountId = "425201"
+accountCity = "Pune"
+
+/*
+ never use var as it has scope problem block scope and functional scope.
+ const makes the variable value non-changeable.
+*/
+console.table([accountPass, accountId, accountName, accountCity]);
